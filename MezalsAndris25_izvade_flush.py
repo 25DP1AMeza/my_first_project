5b102acecfe37b099d1f2da932f5b23d293dcb98
25DP1AMeza <A251079AM@rvt.lv>
2026-03-12 09:54:47 +0200

    Funkcija print ar parametru flush, vērtība pēc noklusējuma (default) ir False.

---
+++ MezalsAndris25_izvade_flush.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from time import sleep
+'''
+
+    Print function with flush parameter
+    Created by : Andris Mežals
+    URL:
+    https://www.includehelp.com/python/flush-parameters-in-python-with-print-function.aspx
+    
+
+'''
+# output is not flushed here
+print('es mācos programmēt ', end='')
+print('lietoju funkciju print ', end='')
+print('ar parametru flush. ', end='')
+
+sleep(5)
+print('Bye! <(0v0)>') 
+input('Press <ENTER> to exit.')

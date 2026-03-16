7a28ff22a9eb8c5da8ae7aa3e1b8a0a2695d2a7b
25DP1AMeza <A251079AM@rvt.lv>
2026-03-16 13:30:57 +0200

    Pievienoju python failu ar ASCII animāciju, kurā bija 3 burti kas rotēja un veidoja animāciju

---
+++ MezalsAndris_26_ASCII_animacija.py
@@ -0,0 +1,35 @@
+#!/usr/bin/enc python
+
+from time import sleep
+
+print("R")
+print(" O")
+print("  T")
+sleep(0.5)
+
+
+print(" R")
+print(" O")
+print(" T")
+
+sleep(0.5)
+
+print("  R")
+print(" O")
+print("T")
+sleep(0.5)
+
+print("T")
+print("O")
+print("R")
+sleep(0.5)
+
+print("  T")
+print(" O")
+print("R")
+sleep(0.5)
+
+print()
+print("ROT")
+
+input("Press <ENTER> to continue..")

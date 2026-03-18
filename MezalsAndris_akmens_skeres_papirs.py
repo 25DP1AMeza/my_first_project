ace344786933891b10e4e20ca884f3a5b32226d8
25DP1AMeza <A251079AM@rvt.lv>
2026-03-18 14:52:36 +0200

    Pievienoju README failu ar akmens skeres papiritis noteikumiem un python failu ar akmens skeres papiritis speli ar ascii prieksmetiem.

---
+++ MezalsAndris_akmens_skeres_papirs.py
@@ -0,0 +1,152 @@
+#Akmens šķēres papīrītis datorspēle
+
+import random
+from time import sleep
+
+datora_izveles = ["""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""","""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""","""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""]
+dators = random.choice(datora_izveles)
+
+
+#ASCII formas
+akmens = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+skeres = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+papirs = """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+print("~~~~~~~~~~Akmens šķēres papīrītis!~~~~~~~~~~")
+print("Akmens = R | Šķēres = S | Papīrs = P")
+
+izvele = input("Kādu priekšmetu jūs izvēlēsieties?").upper()
+
+
+#spēle, ja spēlētājs izvēlas papīru
+if izvele == "P":
+    print(f"Jūs izvēlējāties: {papirs}")
+    sleep(2)
+    print("Dators izvēlējās: ", dators)
+    if dators == """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""":
+        sleep(2)
+        print("Neizšķirts!")
+    elif dators == """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""":
+        sleep(2)
+        print("Jūs uzvarējāt! Apsveicu!")
+    else:
+        sleep(2)
+        print("Jūs zaudējāt :( Vēlu veiksmi nākamajā mēģinājumā!")
+
+
+#spēle, ja spēlētājs izvēlās šķēres
+if izvele == "S":
+    print(f"Jūs izvēlējāties: {skeres}")
+    sleep(2)
+    print("Dators izvēlējās: ", dators)
+    if dators == """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""":
+        sleep(2)
+        print("Jūs uzvarējāt! Apsveicu!")
+    elif dators == """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""":
+        sleep(2)
+        print("Jūs zaudējāt :( Vēlu veiksmi nākamajā mēģinājumā!")
+    else:
+        sleep(2)
+        print("Neizšķirts!")
+
+
+#spēlē, ja spēlētājs izvēlās akmeni
+if izvele == "R":
+    print(f"Jūs izvēlējāties: {akmens}")
+    sleep(2)
+    print("Dators izvēlējās: ", dators)
+    if dators == """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""":
+        sleep(2)
+        print("Jūs zaudējāt :( Vēlu veiksmi nākamajā mēģinājumā!")
+    elif dators == """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""":
+        sleep(2)
+        print("Neizšķirts!")
+    else:
+        sleep(2)
+        print("Jūs uzvarējāt! Apsveicu!")
--- README.md
+++ README.md
@@ -1,40 +1,5 @@
-# Mana sapņu darba vieta
-Andris Mežals DP1-1
-## Par uzņēmumu un darba prasībām:
-### Mikrotīkls
+# Akmens šķēres papīrītis datorspēle
 
+## Noteikumi
 
-
-
-
-Es izvēlējos uzņēmumu SIA "Mikrotīkls",[^1] , jo tas ir moderns IT uzņēmums ar daudz 
-karjeras izaugsmes iespējām un labu atalgojumu. Mikrotīkls arī nodrošina veselības apdrošināšanu, apmaksātas pusdienas un brīvu autostāvietu. Mikrotīkla biroji ir moderni, plaši un nodrošina visu nepieciešamo priekš darba.​
-
-Biroji atrodas Ūnijas ielā 2, Rīgā.
-
-### Mikrotīkla tehnoloģijas,[^1]
-
-Mikrotīkls ražo un veido dažādas tehnoloģijas kā:
-- *Mikrotik RouterOS* programmatūru
-- *RouterBOARD* maršutētājus
-
-Mikrotīkls izmanto C un C++ programmēšanas valodas un Linux, lai izstrādātu Mikrotik RouterOS.
-
-### Prasības [^2]
-
-Šī darba vakance pieprasa ļoti labas zināšanas C un C++  programmēšanas valodā [3], laba izpratne par OS kodola uzbūvi (OS kernel space) [4], pieredze kodola draiveru izstrādnē un teicamas latviešu valodas un angļu valodas zināšanas.
-
-Šis darbs nodrošina mēneša bruto algu no **4000 – 10 000€ mēnesī**, atkarībā no pieredzes.
-
-Šis ir backend programmēšanas darbs.
-
-Pilna slodze.
-
-
-
-
-
-
-[^1]: [Mikrotik. Par uzņēmumu](https://darbs.mikrotik.com/)
-[^2]: [C programmētāja vakance](https://www.cv.lv/lv/vacancy/1487778/mikrotikls-sia/c-programmetajs-a)
-
+Spēli var spēlēt 2 vai vairāk cilvēki (Ieteicams tikai 2. Spēlē ir pieejami tikai 3  objekti, ko var izspēlēt. **akmens, šķēres un papīrs**. **Akmens pārspēj šķēres, šķēres pārspēj papīru un papīrs pārspēj akmeni**. Pirms spēles sākšanas visi spēlētāji saliek plaukstu dūrē. Katrs izdomā, bet skaļi nesaka, kādu objektu izvēlās. Kad katrs ir izvēlējies objektu, ko izspēlēs, tad visi spēlētāji reizē saka **"Akmens - šķēres - papīrītis - viens - divi - trīs!** Uz **trīs** visi spēlētāji parāda savu izvēlēto objektu un izsecina kurš ir uzvarējis. Ja spēlē ar **trīs vai vairāk** spēlētājiem, tad var gadīties situācija, ka katrs ir izvēlējies savādāku priekšmetu. Šāda situācija ir nosaukta par **putru** un neviens nav uzvarējis. Tapēc ir vēlams spēlētājiem sadalīties pa **pāriem** un atbilstošā pāra zaudētājs spēlē ar **cita** pāra zaudētājiem. Pāra uzvarētāji  dara to pašu, bet spēlē ar cita pāra uzvarētājiem.

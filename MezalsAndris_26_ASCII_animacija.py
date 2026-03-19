64838dad1b84518bff3f8e3500882ab919fbd855
25DP1AMeza <A251079AM@rvt.lv>
2026-03-19 09:40:06 +0200

    pievienoju uzlabotu ASCII animācijas python failu, kur trīs burti veic pilnu rotāciju

--- MezalsAndris_26_ASCII_animacija.py
+++ MezalsAndris_26_ASCII_animacija.py
@@ -1,34 +1,49 @@
-#!/usr/bin/enc python
+#!/usr/bin/env python
 
+import os
 from time import sleep
 
+os.system("clear")
 print("R")
 print(" O")
-print("  T")
+print("  T", flush=True)
 sleep(0.5)
 
-
+os.system("clear")
 print(" R")
 print(" O")
-print(" T")
-
+print(" T", flush=True)
 sleep(0.5)
 
+os.system("clear")
 print("  R")
 print(" O")
-print("T")
+print("T", flush=True)
 sleep(0.5)
 
-print("T")
-print("O")
-print("R")
+os.system("clear")
+print()
+print("TOR", flush=True)
 sleep(0.5)
 
+os.system("clear")
+print("T")
+print(" O")
+print("  R", flush=True)
+sleep(0.5)
+
+os.system("clear")
+print(" T")
+print(" O")
+print(" R")
+sleep(0.5)
+
+os.system("clear")
 print("  T")
 print(" O")
 print("R")
 sleep(0.5)
-
+os.system("clear")
 print()
 print("ROT")
 
